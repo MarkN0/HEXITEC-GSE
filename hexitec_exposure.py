587c63962d9ed50d3be96096f678fc483e1558ac
Kyle Gregory <kyle.j.gregory@nasa.gov>
2013-08-06 17:59:44 -0400

    two-ASIC support added
    Implemented support for two ASICs in the FPGA logic. Therefore, the
    exposure object has been upgraded to handle 80x160 images instead of
    80x80.
    
    There seems to be an additional peak in the histogram around 2000 ADU
    that wasn't expected. I am still investigating this.

--- hexitec_exposure.py
+++ hexitec_exposure.py
@@ -33,18 +33,19 @@ class hexitec_exposure:
         #export data set
         #generate calibration table (from data)
         #possibly methods for converting to hyperspectral image and simple spectrum
+        #come up with a way to dynamically scale this object to handle n asics
     #def __new__(self):
         #pass
     
     def __init__(self):
         self.validimages = 0        
-        self.samples = zeros((1000,80,80),int)
+        self.samples = zeros((1000,80,160),int)
         self.rowbufIDs = zeros((1000,80),int)
         self.rowIDs = zeros((1000,80),int)
-        self.mask = zeros((1000,80,80),bool)
+        self.mask = zeros((1000,80,160),bool)
         self.rawflags = zeros((1000,80),int)
         self.endsync = zeros(1000,int)
-        self.caltable = zeros((1000,80,80),int)
+        self.caltable = zeros((80,160),int)
         self.caloffset = 0;
         
         self.caldata = self.loadcaltable()
@@ -53,10 +54,14 @@ class hexitec_exposure:
         self.imgdata.next()
         
     def loadcaltable(self):
-        newcaltable = zeros((80,80),int)        
+        newcaltable = zeros((80,160),int)        
         newcaloffset = (yield)
         for r in range (0,80):
             for c in range (0,20):
+                newcaltable[r][c+140] = (yield)
+                newcaltable[r][c+120] = (yield)
+                newcaltable[r][c+100] = (yield)
+                newcaltable[r][c+80] = (yield)
                 newcaltable[r][c+60] = (yield)
                 newcaltable[r][c+40] = (yield)
                 newcaltable[r][c+20] = (yield)
@@ -67,7 +72,7 @@ class hexitec_exposure:
             (yield)
             
     def loadimagedata(self):
-        inbuffer = zeros(9,int)
+        inbuffer = zeros(17,int)
         while 1:
             while (yield) != 0xFE6B:  #search for sync pattern
                 pass
@@ -76,7 +81,7 @@ class hexitec_exposure:
             #this timestamps
             #timestamps[i] = (buffer[1]%(2**16)) * 2**32 + (buffer[2]%(2**16)) * 2**16 + buffer[3]  #need to solve overflow error...
             for r in range(0,80):
-                for b in range(0,9):    
+                for b in range(0,17):    
                     inbuffer[b] = (yield)
                 self.rowbufIDs[self.validimages][r] = (inbuffer[0] & 0x3c00) >> 10
                 self.rowIDs[self.validimages][r] = inbuffer[0] & 0x00ff
@@ -89,7 +94,15 @@ class hexitec_exposure:
                 self.mask[self.validimages][r][40:54] = dec2bin(inbuffer[6]%(2**14),14)
                 self.mask[self.validimages][r][74:80] = dec2bin(inbuffer[7]%(2**6),6)
                 self.mask[self.validimages][r][60:74] = dec2bin(inbuffer[8]%(2**14),14)
-                for c in range(0,80):
+                self.mask[self.validimages][r][94:100] = dec2bin(inbuffer[9]%(2**6),6)   #need a more efficient way to handle these masks
+                self.mask[self.validimages][r][80:94] = dec2bin(inbuffer[10]%(2**14),14)
+                self.mask[self.validimages][r][114:120] = dec2bin(inbuffer[11]%(2**6),6)
+                self.mask[self.validimages][r][100:114] = dec2bin(inbuffer[12]%(2**14),14)
+                self.mask[self.validimages][r][134:140] = dec2bin(inbuffer[13]%(2**6),6)
+                self.mask[self.validimages][r][120:134] = dec2bin(inbuffer[14]%(2**14),14)
+                self.mask[self.validimages][r][154:160] = dec2bin(inbuffer[15]%(2**6),6)
+                self.mask[self.validimages][r][140:154] = dec2bin(inbuffer[16]%(2**14),14)
+                for c in range(0,160):
                     if self.mask[self.validimages][r][c] == 1:
                         inbuffer[0] = (yield)
                         self.samples[self.validimages][r][c] = inbuffer[0] + ((self.caloffset + self.caltable[r][c]) * (1 - rawflags[i][r]))
